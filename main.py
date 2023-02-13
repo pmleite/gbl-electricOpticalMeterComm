1910199b39697496464512fe1725aea8d6ec4903
pmrleite <paulo.miguel.r.leite@gmail.com>
2023-02-13 15:54:59 -0100

    update

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 from meter import *
 import time
 
-intro()
-hasPorts = get_serial_ports()
 
-if (hasPorts):
-    # Create a meter object
-    meter = meter(init_device())
-    timeStamp = str(time.time()).replace('.','_')
+def runProg():
 
-    # Start hand_shake
-    meter.start_comm()
+    intro()
+    hasPorts = get_serial_ports()
 
-    # Read device info
-    meter.device_info()
-    print(f'\n{INFO_MESSAGES[LANGUAGE]["BRAND"]}\t: {meter.brand}')
-    print(f'{INFO_MESSAGES[LANGUAGE]["MODEL"]}\t: {meter.model}')
-    print(f'{INFO_MESSAGES[LANGUAGE]["ID"]}\t: {meter.id}\n')
+    if (hasPorts):
+        # Create a meter object
+        meter = meter(init_device())
+        timeStamp = str(time.time()).replace('.','_')
 
-    meter.read_data()
+        # Start hand_shake
+        meter.start_comm()
 
-    # Export data to csv
-    fileName = f'{meter.brand}_{meter.model}_{timeStamp}.csv'
-    export_data('csv', fileName)
+        # Read device info
+        meter.device_info()
+        print(f'\n{INFO_MESSAGES[LANGUAGE]["BRAND"]}\t: {meter.brand}')
+        print(f'{INFO_MESSAGES[LANGUAGE]["MODEL"]}\t: {meter.model}')
+        print(f'{INFO_MESSAGES[LANGUAGE]["ID"]}\t: {meter.id}\n')
 
-else:
-    print_message("NO_PORTS")
-    print_message("CHECK")
+        meter.read_data()
+
+        # Export data to csv
+        fileName = f'{meter.brand}_{meter.model}_{timeStamp}.csv'
+        export_data('csv', fileName)
+
+    else:
+        print_message("NO_PORTS")
+        print_message("CHECK")
+
+
+if __name__ == "__main__":
+    runProg()
 
